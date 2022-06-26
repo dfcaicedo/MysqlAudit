2ebe3dbf85c0dd31127253c23897685c802cc27b
Daniel Felipe Caicedo Trujillo <daniel-caicedo@unipiloto.edu.co>
2022-06-26 11:42:09 -0500

    Agregar archivos de proyecto.

---
+++ MysqlAudit/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Host"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "User"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Password"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "DataBase"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 117)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 35)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Probar conexión"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(116, 117)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 35)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Generar Script"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(91, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(91, 33)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(91, 59)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(91, 84)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(224, 169)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "MysqlAudit"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+
+End Class
---
+++ MysqlAudit/Form1.vb
@@ -0,0 +1,133 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
+Public Class Form1
+    Dim cnt As MySqlConnection
+    Dim cnt2 As MySqlConnection
+    Dim host As String
+    Dim user As String
+    Dim pass As String
+    Dim bdd As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+            Me.host = Me.TextBox1.Text
+            Me.user = Me.TextBox2.Text
+            Me.pass = Me.TextBox3.Text
+            Me.bdd = Me.TextBox4.Text
+            cnt = New MySqlConnection("Server=" & Me.host & ";Database=" & Me.bdd & ";Uid= " & Me.user & ";Pwd=" & Me.pass & ";")
+            cnt2 = New MySqlConnection("Server=" & Me.host & ";Database=" & Me.bdd & ";Uid= " & Me.user & ";Pwd=" & Me.pass & ";")
+            cnt.Open()
+            cnt.Close()
+            MsgBox("La conexión ha sido exitosa")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim sql As String
+        Dim ltablas As MySqlDataReader
+        Dim lcampos As MySqlDataReader
+        Dim cmdt As MySqlCommand
+        Dim cmdc As MySqlCommand
+        Button1_Click(sender, e)
+        Try
+            cnt.Open()
+            sql = " drop table if EXISTS  au_dtran;" & vbCrLf
+            sql &= "drop table if EXISTS  au_trans;" & vbCrLf
+            sql &= "CREATE TABLE IF NOT EXISTS `au_trans` (" & vbCrLf
+            sql &= "    `tra_cont` bigint(20) NOT NULL AUTO_INCREMENT," & vbCrLf
+            sql &= "    `tra_tabl` varchar(500) NOT NULL," & vbCrLf
+            sql &= "    `tra_acci` varchar(50) NOT NULL," & vbCrLf
+            sql &= "    `tra_user` varchar(50) NOT NULL," & vbCrLf
+            sql &= "    `tra_fech` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP," & vbCrLf
+            sql &= "    PRIMARY KEY (`tra_cont`)" & vbCrLf
+            sql &= ") ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;" & vbCrLf
+            sql &= "CREATE TABLE IF NOT EXISTS `au_dtran` (" & vbCrLf
+            sql &= "    `dtr_cont` bigint(20) NOT NULL AUTO_INCREMENT," & vbCrLf
+            sql &= "    `tra_cont` bigint(20) NOT NULL," & vbCrLf
+            sql &= "    `dtr_fiel` varchar(1500)  NULL," & vbCrLf
+            sql &= "    `dtr_vnew` varchar(1500)  NULL," & vbCrLf
+            sql &= "    `dtr_vold` varchar(1500)  NULL," & vbCrLf
+            sql &= "    PRIMARY KEY (`dtr_cont`)" & vbCrLf
+            sql &= ") ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;" & vbCrLf
+            sql &= "delimiter //" & vbCrLf
+            sql &= "drop trigger if exists eliminarduplicados_trg //" & vbCrLf
+            sql &= "ALTER TABLE `au_dtran`" & vbCrLf
+            sql &= "ADD INDEX `transacion` (`tra_cont` ASC);" & vbCrLf
+            sql &= "ALTER TABLE `spadb`.`au_dtran` " & vbCrLf
+            sql &= "ADD CONSTRAINT `transacion_fk`" & vbCrLf
+            sql &= "FOREIGN KEY (`tra_cont`)" & vbCrLf
+            sql &= "REFERENCES `spadb`.`au_trans` (`tra_cont`)" & vbCrLf
+            sql &= "ON DELETE cascade" & vbCrLf
+            sql &= "ON UPDATE NO ACTION;" & vbCrLf
+
+            cmdt = New MySqlCommand("show full tables where table_type ='BASE TABLE'", Me.cnt)
+            ltablas = cmdt.ExecuteReader
+            While ltablas.Read
+                Dim tabla As String
+                tabla = ltablas.GetValue(0)
+                If tabla <> "au_trans" And tabla <> "au_dtran" Then
+                    cnt2.Open()
+                    cmdc = New MySqlCommand("desc " & tabla, cnt2)
+                    Dim campos() As String
+                    campos = Nothing
+                    lcampos = cmdc.ExecuteReader
+                    While lcampos.Read
+                        If campos Is Nothing Then
+                            ReDim campos(0)
+                        Else
+                            ReDim Preserve campos(campos.Length)
+                        End If
+                        campos(campos.Length - 1) = lcampos.GetValue(0)
+                    End While
+                    lcampos.Close()
+                    cnt2.Close()
+                    sql &= "drop trigger if exists " & tabla & "_insert_audit //" & vbCrLf
+                    sql &= " create trigger " & tabla & "_insert_audit after insert on " & tabla & vbCrLf
+                    sql &= "    for each row" & vbCrLf
+                    sql &= "    begin" & vbCrLf
+                    sql &= "        insert into au_trans values(null,'" & tabla & "','INSERT',USER(),now());" & vbCrLf
+                    For p = 0 To campos.Length - 1
+                        sql &= "        insert into au_dtran values(null,(select max(tra_cont) from au_trans),'" & campos(p) & "',new." & campos(p) & ",'');" & vbCrLf
+                    Next
+                    sql &= " end " & vbCrLf
+                    sql &= "//" & vbCrLf
+                    sql &= "drop trigger if exists " & tabla & "_update_audit //" & vbCrLf
+                    sql &= " create trigger " & tabla & "_update_audit before update on " & tabla & vbCrLf
+                    sql &= "    for each row" & vbCrLf
+                    sql &= "    begin" & vbCrLf
+                    sql &= "        insert into au_trans values(null,'" & tabla & "','UPDATE',USER(),now());" & vbCrLf
+                    For p = 0 To campos.Length - 1
+                        sql &= " if New." & campos(p) & " <> Old." & campos(p) & " then" & vbNewLine
+                        sql &= "        insert into au_dtran values(null,(select max(tra_cont) from au_trans),'" & campos(p) & "',new." & campos(p) & ",old." & campos(p) & ");" & vbCrLf
+                        sql &= "end if;" & vbNewLine
+                    Next
+                    sql &= " end " & vbCrLf
+                    sql &= "//" & vbCrLf
+                    sql &= "drop trigger if exists " & tabla & "_delete_audit //" & vbCrLf
+                    sql &= " create trigger " & tabla & "_delete_audit before delete on " & tabla & vbCrLf
+                    sql &= "    for each row" & vbCrLf
+                    sql &= "    begin" & vbCrLf
+                    sql &= "        insert into au_trans values(null,'" & tabla & "','DELETE',USER(),now());" & vbCrLf
+                    For p = 0 To campos.Length - 1
+                        sql &= "        insert into au_dtran values(null,(select max(tra_cont) from au_trans),'" & campos(p) & "','',old." & campos(p) & ");" & vbCrLf
+                    Next
+                    sql &= " end " & vbCrLf
+                    sql &= "//" & vbCrLf
+                End If
+            End While
+            ltablas.Close()
+            Me.cnt.Close()
+            Dim qq As StreamWriter
+            qq = New StreamWriter("auditscript.sql")
+            sql &= "delimiter ;" & vbCrLf
+            qq.Write(sql)
+            qq.Close()
+            Shell("explorer.exe auditscript.sql")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+End Class
